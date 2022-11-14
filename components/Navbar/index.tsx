ab67370d2c586a27652b874dd88bf0878e707aec
ShivamBh <shivam.bhattacharjee94@gmail.com>
2022-11-14 21:44:22 +0530

    init navbar

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <>
+      <div className="navbar-main text-slate-50">
+        <div className="navbar-wrapper flex justify-between text-xl align-top">
+          <div className="navbar-branding ">
+            <p className="logo-text text-8xl">SHIBUJOE</p>
+          </div>
+          <div className="navbar-menu">
+            <p className="menu-text">INDEX</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
