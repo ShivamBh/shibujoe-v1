b55fe999be9a0220141ab8c48651a44b1c3ce2fd
ShivamBh <shivam.bhattacharjee94@gmail.com>
2022-11-14 21:44:57 +0530

    intro section

---
+++ components/Sections/Introduction.tsx
@@ -0,0 +1,16 @@
+function Introduction() {
+  return (
+    <section className="intro py-8 flex justify-end gap-8">
+      <div className="intro-spacer w-2/5"></div>
+      <div className="intro-wrapper w-3/5 text-6xl text-slate-50 p-8">
+        <h1 className="intro-text leading-tight uppercase">
+          <span className="intro-text-line block">
+            Solid Digital Experiences require the craft of design.
+          </span>
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default Introduction;
