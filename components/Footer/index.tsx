933cc6075ed3680f696657f40e32f9d8f03635d1
ShivamBh <shivam.bhattacharjee94@gmail.com>
2022-11-14 21:44:28 +0530

    init footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div className="footer-main h-80 bg-slate-50">
+      <div className="footer-item p-8">
+        <h3 className="text-3xl">FOOTER</h3>
+        <h3 className="text-3xl">CONTENT</h3>
+        <h3 className="text-3xl">HERE</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
