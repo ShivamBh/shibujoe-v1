99bf4702a46ba778b08d081cc59e9e3e8137f36d
ShivamBh <shivam.bhattacharjee94@gmail.com>
2022-11-15 09:07:09 +0530

    added Lenis for smooth scroll

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,9 +1,30 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useLayoutEffect } from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import SiteContainer from "../SiteContainer";
+import Lenis from "@studio-freight/lenis";
 
 function DefaultLayout({ children }: { children: ReactNode }) {
+  useLayoutEffect(() => {
+    const lenis = new Lenis({
+      duration: 3,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: "vertical", // vertical, horizontal
+      gestureDirection: "vertical", // vertical, horizontal, both
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <>
       <SiteContainer>
