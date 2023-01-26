93d87f93963e2e0fe273d8d85c5b70eebb2dced5
ShivamBh <shivam.bhattacharjee94@gmail.com>
2023-01-26 15:53:02 +0530

    import global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
